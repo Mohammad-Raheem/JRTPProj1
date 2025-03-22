0674c30e038b61c499a12f2f8203d8c18d3b385b
Mohammad-Raheem <raheemmohammad0410@gmail.com>
2025-03-22 14:51:09 +0530

    m7 created

--- test1.java
+++ test1.java
@@ -2,8 +2,13 @@ public class ClassOne{
 	public void metd1(){
 		System.out.println("Hello World::");
 	}
+<<<<<<< Updated upstream
 	public void metd3(){
 		System.out.println("this is method-3");
+=======
+	public void metd2(){
+		System.out.println("this is method::");
+>>>>>>> Stashed changes
 	}
 	
 }
